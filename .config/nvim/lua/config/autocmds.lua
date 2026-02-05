d20f65af33c4c3570ae64d60fcfa058a332da106
Szu <linken0323@gmail.com>
2026-02-04 19:06:06 -0500

    Lazy-Lock Update

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,7 @@
+-- Fix ConcealLevel For Json
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json", "jsonc", "json5" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
