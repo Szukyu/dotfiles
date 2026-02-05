35d7ec4ca273f0da3eae1fd69db3b009fee77af7
Szu <linken0323@gmail.com>
2026-02-04 19:06:07 -0500

    Halved Nvim Startup Time

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -15,18 +15,40 @@ require("config.options")
 require("config.autocmds")
 require("config.keymaps")
 require("lazy").setup({ import = "plugins" }, {
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
-	ui = {
-		size = {
-			width = 0.8,
-			height = 0.8,
-		},
-		border = "rounded",
-	},
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+  defaults = {
+    lazy = true,
+  },
+  ui = {
+    size = {
+      width = 0.8,
+      height = 0.8,
+    },
+    border = "rounded",
+  },
+  performance = {
+    cache = {
+      enabled = true,
+    },
+    reset_packpath = true,
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      }
+    }
+  },
+  debug = false
 })
