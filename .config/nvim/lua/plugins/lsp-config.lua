06dc6a4f3d505f11d4b921ac66bfdf9af9be8a69
Szu <linken0323@gmail.com>
2026-02-04 19:06:06 -0500

    Change to zshrc, because zoxide sucks on nushell

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -42,7 +42,7 @@ return {
   config = function (_, opts)
     vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(ev)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = ev.buf })
+        vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = ev.buf })
       end
     })
 
