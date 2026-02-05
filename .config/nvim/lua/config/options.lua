484a069d2f987950a47655010a6ba9d6f7e206a3
Szu <linken0323@gmail.com>
2026-02-04 19:06:07 -0500

    Added a lot of options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -15,13 +15,31 @@ opt.fillchars = {
   diff = "╱",
   eob = " ",
 }
+opt.formatoptions = "jcroqlnt"
+opt.grepformat = "%f:%l:%c:%m"
+opt.grepprg = "rg --vimgrep"
 opt.ignorecase = true
+opt.linebreak = true
+opt.pumblend = 10
+opt.ruler = false
 opt.shiftround = true
 opt.shiftwidth = 2
 opt.showmode = false
 opt.signcolumn = "yes"
+opt.smartcase = true
+opt.smartindent = true
+opt.spelllang = { "en" }
+opt.splitbelow = true -- Put new windows below current
+opt.splitkeep = "screen"
+opt.splitright = true -- Put new windows right of current
+opt.statuscolumn = [[%!v:lua.require'snacks.statuscolumn'.get()]]
 opt.tabstop = 2
+opt.undolevels = 10000
+opt.updatetime = 200
+opt.virtualedit = "block"
+opt.wildmode = "longest:full,full"
 opt.termguicolors = true
+opt.wrap = false
 
 -- Keymaps Silent By Default
 local keymap_set = vim.keymap.set
@@ -30,6 +48,3 @@ vim.keymap.set = function(mode, lhs, rhs, opts)
   opts.silent = opts.silent ~= false
   return keymap_set(mode, lhs, rhs, opts)
 end
-
--- Fix markdown indentation settings
-vim.g.markdown_recommended_style = 0
