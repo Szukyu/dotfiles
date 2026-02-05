0fddb3d4eff87d894a75bbe1211050e792168e85
Szu <linken0323@gmail.com>
2026-02-04 19:06:07 -0500

    Change LSP Sign as it was deprecated

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -50,11 +50,16 @@ return {
       end
     })
 
-    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = " ",
+          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.INFO] = "󰋼 ",
+          [vim.diagnostic.severity.HINT] = "󰌵 ",
+        },
+      },
+    })
 
     local lspconfig = require('lspconfig')
     for server, config in pairs(opts.servers) do
