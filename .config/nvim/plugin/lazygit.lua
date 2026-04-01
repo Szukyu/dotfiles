27c4350e643823f2bda7290617a84e8b4f159dcf
tsukuyomi <linken0323@gmail.com>
2026-04-01 14:14:49 -0400

    Fixed lazygit in nvim renaminng

--- .config/nvim/plugin/lazygit.lua
+++ .config/nvim/plugin/lazygit.lua
@@ -1,23 +1,18 @@
 local open_lazygit = function()
-  vim.cmd('tabedit') 
+  vim.cmd('tabedit')
   vim.opt_local.number = false
   vim.opt_local.relativenumber = false
   vim.opt_local.signcolumn = 'no'
 
-  -- Get git paths safely
-  local git_dir = vim.fn.system('git rev-parse --git-dir'):gsub('\n', '')
-  local work_tree = vim.fn.getcwd()
-
-  -- Run lazygit
-  vim.fn.termopen(string.format('env VIMRUNTIME= VIM= lazygit --git-dir=%s --work-tree=%s', git_dir, work_tree), {
+  vim.fn.termopen('env VIMRUNTIME= VIM= lazygit', {
     on_exit = function()
-      -- Close the tab/buffer when lazygit exits
       vim.cmd('silent! checktime')
       vim.cmd('silent! bwipeout!')
     end,
   })
 
   vim.cmd('startinsert')
+  vim.b.minipairs_disable = true
 end
 
 vim.keymap.set('n', '<leader>gg', open_lazygit, { desc = 'Git tab', silent = true })
