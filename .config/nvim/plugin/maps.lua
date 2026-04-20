71b32932d9ddccf2dc7e8767aba09256a859329c
tsukuyomi <linken0323@gmail.com>
2026-04-20 13:05:01 -0400

    Walls + Small additions

--- .config/nvim/plugin/maps.lua
+++ .config/nvim/plugin/maps.lua
@@ -5,6 +5,8 @@ map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true
 map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" })
 
 map("n", "<leader>e", function() MiniFiles.open() end, { desc = "Find Files" })
 map("n", "<leader><space>", function() require('fzf-lua').files() end, { desc = "Fzf Files" })
