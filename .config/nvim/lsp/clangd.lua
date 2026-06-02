0f0398f28fef296c3134e3d5758cce2de0d79358
Szukyu <linken0323@gmail.com>
2026-06-02 12:32:20 -0400

    Added keymap for yazi + update clangd lsp

--- .config/nvim/lsp/clangd.lua
+++ .config/nvim/lsp/clangd.lua
@@ -1,6 +1,19 @@
 return {
+	cmd = { "clangd", "--background-index", "--clang-tidy" },
 	filetypes = {
 		"c",
-		"cpp"
-	}
+		"cpp",
+		"objc",
+		"objcpp",
+		"cuda",
+		"proto"
+	},
+	root_markers = {
+		".clangd",
+		".clang-format",
+		"compile_commands.json",
+		"compile_flags.txt",
+		"configure.ac",
+		".git",
+	},
 }
